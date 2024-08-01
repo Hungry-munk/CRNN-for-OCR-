83e5f52bf8da4faf4c1b185c16a329fa510b5fdd
hungry_munk <abbaszadeharsam@gmail.com>
2024-08-01 17:11:59 +1000

    added configs file and delted previous data preprcessing functions

---
+++ configs.py
@@ -0,0 +1,12 @@
+class modelConfigs:
+    """ 
+    A class created purely to store some variable values.
+    The class is to be used purely for image preprocessing and model training (variable storage) 
+    """
+    def __init__ (self):
+        self.augmentation_probability  = 0.3
+        self.epoch_num = 200 #TODO change value to something that makes sense
+        self.height = 100 #TODO figure out a height that makes sense
+        self.batch_size = 1024 #TODO change to something that makes sense
+        self.learning_rate = 1e-4 #may not need thanks to ADAM optimzer
+
