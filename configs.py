314706067f69f2c314010da1dc5cf91731a20c7e
Hungry_munk <abbaszadeharsam@gmail.com>
2024-08-10 12:37:37 +1000

    cahnged image configs slightly and built batch displaying function

--- configs.py
+++ configs.py
@@ -12,24 +12,9 @@ class modelConfigs:
         self.augmentation_probability  = 0.3
         self.data_split = (0.4, 0.6)
         self.epoch_num = 200 #TODO change value to something that makes sense
-        self.image_height = 256 #TODO figure out a height that makes sense
+        self.image_height = 512 #TODO figure out a height that makes sense
         self.batch_size = 1024 #TODO change to something that makes sense
         self.learning_rate = 1e-4 #may not need thanks to ADAM optimzer
-        self.letter_to_index = {
-            1: 'a',
-            2: 'b',
-            3: 'c',
-            4: 'd',
-            5: 'e',
-            6: 'f',
-            7: 'g',
-            8: 'h',
-            9: 'j',
-            10: 'k',
-            11: 'l',
-            12: 'm',
-            13: 'n',
-            14: 'p',
-            15: 'r'
-        }
 
+        #temp attributes
+        self.test_batch_size = 100
